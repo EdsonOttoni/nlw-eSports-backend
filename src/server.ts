c7df9a0ec33ad839dcce07660738ad32d7dbbdeb
EdsonOttoni <edson.ottoni@gmail.com>
2022-09-22 17:00:10 -0300

    Add prisma

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from "express" 
 
-const app = express()
+const app = express() 
 
 app.get('/', (req, res) => {
   return res.send('Acessou ads!')
 })
 
-app.listen(3333)
+app.listen(3333) 
